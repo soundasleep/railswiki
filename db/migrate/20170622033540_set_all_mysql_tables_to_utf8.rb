41872d3d2a279cd67b8bab375a447f4f405c3a27
Jevon Wright <jevon@powershop.co.nz>
2017-06-22 15:51:24 +1200

    When changing tables to UTF8, we need to also change strings to VARCHAR(191)
    See http://blog.arkency.com/2015/05/how-to-store-emoji-in-a-rails-app-with-a-mysql-database/

--- db/migrate/20170622033540_set_all_mysql_tables_to_utf8.rb
+++ db/migrate/20170622033540_set_all_mysql_tables_to_utf8.rb
@@ -1,8 +1,39 @@
 class SetAllMysqlTablesToUtf8 < ActiveRecord::Migration[5.1]
+  MAX_VARCHAR_LENGTH = 191
+
   def change
+    # we have to reduce the length of varchar(255) to varchar(191)
+    # so it fits in 255 bytes with UTF8 for MySQL
+    change_table :railswiki_users do |t|
+      t.change :provider,      :string, limit: MAX_VARCHAR_LENGTH
+      t.change :uid,           :string, limit: MAX_VARCHAR_LENGTH
+      t.change :name,          :string, limit: MAX_VARCHAR_LENGTH
+      t.change :refresh_token, :string, limit: MAX_VARCHAR_LENGTH
+      t.change :access_token,  :string, limit: MAX_VARCHAR_LENGTH
+      t.change :email,         :string, limit: MAX_VARCHAR_LENGTH
+      t.change :image_url,     :string, limit: MAX_VARCHAR_LENGTH
+      t.change :role,          :string, limit: MAX_VARCHAR_LENGTH
+    end
+
+    change_table :railswiki_invites do |t|
+      t.change :email,         :string, limit: MAX_VARCHAR_LENGTH
+      t.change :role,          :string, limit: MAX_VARCHAR_LENGTH
+    end
+
+    change_table :railswiki_pages do |t|
+      t.change :title,         :string, limit: MAX_VARCHAR_LENGTH
+      t.change :lowercase_title, :string, limit: MAX_VARCHAR_LENGTH
+    end
+
+    change_table :railswiki_uploaded_files do |t|
+      t.change :file,          :string, limit: MAX_VARCHAR_LENGTH
+      t.change :title,         :string, limit: MAX_VARCHAR_LENGTH
+    end
+
     adapter_type = connection.adapter_name.downcase.to_sym
     case adapter_type
     when :mysql, :mysql2
+      # then change the character sets
       [
         "railswiki_histories",
         "railswiki_invites",
@@ -10,7 +41,7 @@ class SetAllMysqlTablesToUtf8 < ActiveRecord::Migration[5.1]
         "railswiki_uploaded_files",
         "railswiki_users"
       ].each do |table|
-        Railswiki::Page.connection.execute "ALTER TABLE #{table} CONVERT TO CHARACTER SET utf8mb4 COLLATe utf8mb4_bin;"
+        Railswiki::Page.connection.execute "ALTER TABLE #{table} CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
       end
     else
       puts "Ignoring adapter #{adapter_type}"
