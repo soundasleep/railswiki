0f7ca14504eda685fd98667605445c938f71ff67
Jevon Wright <jevon@powershop.co.nz>
2017-09-21 11:43:21 +1200

    Allow inline images to be sized by %

--- app/helpers/railswiki/wiki_helper.rb
+++ app/helpers/railswiki/wiki_helper.rb
@@ -138,10 +138,16 @@ module Railswiki
       if link =~ /^(.+?)\s*=+(\d+)(?:px|)$/
         # ![alt](url.png =100px)
         # ![alt](url.png =100)
-        %(<img src="#{$1}" style="max-width: #{$2}px" alt="#{alt_text}">)
+        %(<img src="#{$1}" style="max-width: #{$2}px;" alt="#{alt_text}">)
       elsif link =~ /^(.+?)\s*=+(\d+)(?:px|)x(\d+)(?:px|)$/
         # ![alt](url.png =30x50)
         %(<img src="#{$1}" style="max-width: #{$2}px; max-height: #{$3}px;" alt="#{alt_text}">)
+      elsif link =~ /^(.+?)\s*=+(\d+)%$/
+        # ![alt](url.png =50%)
+        %(<img src="#{$1}" style="max-width: #{$2}%;" alt="#{alt_text}">)
+      elsif link =~ /^(.+?)\s*=+(\d+)%x(\d+)%$/
+        # ![alt](url.png =50%x50%)
+        %(<img src="#{$1}" style="max-width: #{$2}%; max-height: #{$3}%;" alt="#{alt_text}">)
       else
         %(<img src="#{link}" title="#{title}" alt="#{alt_text}">)
       end
