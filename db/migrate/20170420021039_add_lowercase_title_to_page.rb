b201c5c103ce538a3a7f30026c6f3ab2cb9f8d5a
Jevon Wright <jevon@jevon.org>
2018-02-19 12:16:34 +1300

    A new NOT NULL column in a table must have a default value
    This works fine on MySQL but Sqlite3 complains

--- db/migrate/20170420021039_add_lowercase_title_to_page.rb
+++ db/migrate/20170420021039_add_lowercase_title_to_page.rb
@@ -1,5 +1,6 @@
 class AddLowercaseTitleToPage < ActiveRecord::Migration[5.0]
   def change
-    add_column :railswiki_pages, :lowercase_title, :string, null: false, unique: true
+    # We need to have a default value for new columns even if they are NOT NULL
+    add_column :railswiki_pages, :lowercase_title, :string, null: false, unique: true, default: "should not be set"
   end
 end
